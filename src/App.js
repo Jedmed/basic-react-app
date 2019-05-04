d65019cad872c165e66ec88a5ce91fd758868b9a
Cylis Vaidya <cylisvaidya@Cylis-MacBook-Pro-13.local>
2019-05-04 19:25:45 +0700

    conflict

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
+import Cylis from './cylis/Cylis';
+import Asset from './cylis/Asset'
 
-class App extends Component {
-  render() {
-    return <h1>My React App</h1>;
+// const Assets = ['Property', 'Car', 'Collection', 'Invention'];
+
+class App extends PureComponent {
+	state = {
+		asset: ['Property', 'Car', 'Collection', 'Invention'],
+	};
+
+	addAsset(value) {
+		const asset = this.state.asset;
+		const newAsset = [...asset, value];
+		this.setState({ asset: newAsset });
+	}
+
+	render() {
+	return (
+		<div>
+			<section className="header">
+				<h1>My React App</h1>
+				<p>Description of this app</p>
+				<div className="authorinfo">
+					<Cylis surname="Vaidya" />
+				</div>
+			</section>
+			<section className="asset-types">
+				<ul>
+					{this.state.asset.map(value => (
+						<Asset name={value} />
+					))}
+				</ul>
+				<button type="button" onClick={() => this.addAsset('Testing')}>
+					Click me!
+				</button>
+			</section>
+		</div>
+    );
   }
 }
 
