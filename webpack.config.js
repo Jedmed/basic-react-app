94a70a8c3db2931cfe71592aa63aaf84e8e615b2
Joseph Edmed <joseph@ekoapp.com>
2019-05-04 17:26:50 +0700

    working version

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,59 @@
-const path = require("path");
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const path = require('path');
+const webpack = require('webpack');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 
+const dev = process.env.NODE_ENV !== 'production';
+
+const HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
+  template: path.join(__dirname, '/src/index.html'),
+  filename: 'index.html',
+  inject: 'body',
+});
+
+const DefinePluginConfig = new webpack.DefinePlugin({
+  'process.env.NODE_ENV': JSON.stringify('production'),
+});
 
 module.exports = {
-    devtool: 'source-map',
-    entry: "./src/index.js",
-    output: {
-        path: path.join(__dirname, "/dist"),
-        filename: "index_bundle.js"
+  devServer: {
+    host: 'localhost',
+    port: '3000',
+    hot: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
     },
-    module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/preset-react']
-                }
-            }
-        }, {
-            test: /\.css$/,
-            use: ExtractTextPlugin.extract(
-                {
-                    fallback: 'style-loader',
-                    use: ['css-loader']
-                }
-            )
+    historyApiFallback: true,
+  },
+  entry: ['@babel/polyfill', 'whatwg-fetch', 'react-hot-loader/patch', path.join(__dirname, '/src/index.jsx')],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loaders: ['babel-loader'],
+      },
+      {
+        test: /\.scss$/,
+        loader: 'style-loader!css-loader!sass-loader',
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
         },
-        {
-            test: /\.(png|jpg|gif)$/,
-            use: [
-                {
-                    loader: 'file-loader'
-                }
-            ]
-        }
-        ]
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            hash: true,
-            filename: "index.html",  //target html
-            template: "./src/public/index.html" //source html
-        }),
-        new ExtractTextPlugin({ filename: 'css/style.css' })
-    ]
-}
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  output: {
+    filename: 'index.js',
+    path: path.join(__dirname, '/build'),
+  },
+  mode: dev ? 'development' : 'production',
+  plugins: dev
+    ? [HTMLWebpackPluginConfig, new webpack.HotModuleReplacementPlugin()]
+    : [HTMLWebpackPluginConfig, DefinePluginConfig],
+};
