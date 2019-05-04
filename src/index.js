44163d0f87fd3d0c1f01d6dc7a2932b49427608a
Joseph Edmed <joseph@ekoapp.com>
2019-05-04 17:38:21 +0700

    fixed things

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+import './style.scss';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// if (module.hot) {
+//   module.hot.accept('./App', () => {
+//     render(App);
+//   });
+// }
