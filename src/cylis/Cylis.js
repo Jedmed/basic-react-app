3d7ff9c043cff2c81b60a1bff38caf7b3ede62e9
Cylis Vaidya <cylisvaidya@Cylis-MacBook-Pro-13.local>
2019-05-04 19:15:42 +0700

    my first commit

---
+++ src/cylis/Cylis.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Cylis extends React.Component{
+    render() {
+        return(
+            <p>I am Cylis {this.props.surname}</p>
+        );
+    }
+}
+
+export default Cylis;
